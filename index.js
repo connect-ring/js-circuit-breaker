90da69040cc6fe9b9b848e5e9001281eb6595c0d
gsuzuki983 <48849186+gsuzuki983@users.noreply.github.com>
2022-08-25 14:56:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const CircuitBreaker = require('opossum');
+
+// サーキットブレーカーの対象となる非同期関数を実装
+const asyncFunction = (arg) => {
+  return new Promise((resolve, reject) => {
+    arg === 'OK' ? resolve(`${arg}!!!`) : reject();
+  });
+};
+
+// opossumに引数で実装した非同期関数を渡す
+const circuit = new CircuitBreaker(asyncFunction);
+
+// fallbackで非同期関数でエラーが発生した場合の処理を実装
+circuit.fallback((arg) => {
+  return `${arg}!!!`
+});
+
+(async () => {
+  const ret = await Promise.all([
+    circuit.fire("OK"),
+    circuit.fire("NG"),
+    circuit.fire("OK")
+  ]);
+  console.log(ret);
+})();
